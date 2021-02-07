4aa112ce93080946b0a8c840f77cd9305fed36b0
Johnny <1249864985@qq.com>
2021-02-07 09:39:06 +0800

    admin模块新增查询方法

--- vis-cloud-admin/src/main/java/com/johnny/vis/cloud/admin/controller/ServerController.java
+++ vis-cloud-admin/src/main/java/com/johnny/vis/cloud/admin/controller/ServerController.java
@@ -1,9 +1,17 @@
 package com.johnny.vis.cloud.admin.controller;
 
+import com.johnny.vis.cloud.admin.entity.ServerEntity;
 import com.johnny.vis.cloud.admin.service.ServerService;
+import com.johnny.vis.cloud.common.constant.Response;
+import com.johnny.vis.cloud.common.entity.ResponseData;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author Johnny
@@ -17,5 +25,18 @@ public class ServerController {
 
     public ServerController(ServerService serverService) {
         this.serverService = serverService;
+    }
+
+    @ApiOperation("保存服务器信息")
+    @PostMapping("saveServer")
+    public ResponseData<Object> saveServer(ServerEntity entity) {
+        this.serverService.saveOrUpdate(entity);
+        return ResponseData.success(Response.SUCCESS_SAVE);
+    }
+
+    @ApiOperation("条件查询服务器信息列表")
+    @GetMapping("queryServerList")
+    public ResponseData<List<ServerEntity>> queryServerList(ServerEntity entity) {
+        return ResponseData.success(serverService.queryServerList(entity));
     }
 }
--- vis-cloud-admin/src/main/java/com/johnny/vis/cloud/admin/service/ServerService.java
+++ vis-cloud-admin/src/main/java/com/johnny/vis/cloud/admin/service/ServerService.java
@@ -1,9 +1,12 @@
 package com.johnny.vis.cloud.admin.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.johnny.vis.cloud.admin.dao.ServerMapper;
 import com.johnny.vis.cloud.admin.entity.ServerEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author Johnny
@@ -11,4 +14,13 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ServerService extends ServiceImpl<ServerMapper, ServerEntity> {
+
+    /**
+     * 条件查询服务器信息列表
+     *
+     * @param entity entity
+     */
+    public List<ServerEntity> queryServerList(ServerEntity entity) {
+        return this.baseMapper.selectList(new QueryWrapper<ServerEntity>().lambda());
+    }
 }
