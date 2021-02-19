343e7804c6b4776bef94e788b9f2aff44deaa3fd
johnny <1249864985@qq.com>
2021-02-19 06:40:02 +0000

    调整发布配置

---
+++ vis-cloud-admin/build.sh
@@ -0,0 +1,17 @@
+docker_user="vis"
+docker_pwd="visPower@2021"
+docker_project="vis"
+docker_domain="ub20-docker.com"
+
+echo "maven version: \c"
+mvn -v
+
+mvn clean
+mvn package
+mvn docker:build
+
+docker login --username=${docker_user} --password=${docker_pwd} ${docker_domain}
+
+docker image ls
+
+docker push ${docker_domain}/${docker_project}/vis-cloud-admin:0.0.1
